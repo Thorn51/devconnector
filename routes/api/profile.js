2e2b41e0886b75ab308c36121b62d1ca29b9afd6
Matt Thornberry <mtberry16@hotmail.com>
2020-06-19 08:31:20 -0600

    Add route to retreive GitHub repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-const { check, validationResult } = require("express-validator");
+const { check, validationResult, body } = require("express-validator");
 const normalize = require("normalize-url");
 const xss = require("xss");
+const request = require("request");
+const config = require("config");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { response } = require("express");
 
 // @route   GET api/profile/me
 // @desc    Get the current users profile based on id
@@ -310,6 +313,37 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Retrieve user GitHub repos
+// @access  public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ message: "No Github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    res.status(500).send("Server Error");
+    console.error(err.message);
   }
 });
 module.exports = router;
