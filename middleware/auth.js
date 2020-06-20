05909f3257b6d00d7f4f7d63cb32b56ec09e9ed3
Matt Thornberry <mtberry16@hotmail.com>
2020-06-20 08:40:48 -0600

    Fix missing return

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
 
   // Check if no token
   if (!token) {
-    res.status(401).json({ message: "No token, authorization denied" });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
