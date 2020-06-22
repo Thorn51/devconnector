db6bd44076d4a3ec7226b25966fa30217c41214f
Matt Thornberry <mtberry16@hotmail.com>
2020-06-22 08:09:17 -0600

    Import landing and navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
+import Navbar from "../src/components/layout/Navbar";
+import Landing from "../src/components/layout/Landing";
 import "./App.css";
 
 const App = () => {
   return (
     <Fragment>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 };
