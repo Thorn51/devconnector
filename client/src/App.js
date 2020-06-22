38c86b96a257620116cbd762cfe8dee8b5c263a7
Matt Thornberry <mtberry16@hotmail.com>
2020-06-22 10:15:49 -0600

    Add Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../src/components/layout/Navbar";
 import Landing from "../src/components/layout/Landing";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+      </Fragment>
+    </Router>
   );
 };
 
