b5edd0a6d97705f4cc022a3b802ed6e4d14276cc
Matt Thornberry <mtberry16@hotmail.com>
2020-06-23 10:22:04 -0600

    Add login and register componenets

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../src/components/layout/Navbar";
 import Landing from "../src/components/layout/Landing";
+import Login from "../src/components/auth/Login";
+import Register from "../src/components/auth/Register";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +12,12 @@ const App = () => {
       <Fragment>
         <Navbar />
         <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
