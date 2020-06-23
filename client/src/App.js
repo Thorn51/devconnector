5cf9a30bc9af0afc8401a340bec8eeb022ea9ced
Matt Thornberry <mtberry16@hotmail.com>
2020-06-23 12:03:13 -0600

    Add provider and redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,26 @@ import Landing from "../src/components/layout/Landing";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
