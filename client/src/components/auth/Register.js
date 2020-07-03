42151fc8345c2039eda6d1ce8bf92fa7f0fca37a
Matt Thornberry <mtberry16@hotmail.com>
2020-07-03 09:46:09 -0600

    Add prop types

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
 const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
@@ -84,6 +85,10 @@ const Register = ({ setAlert }) => {
       </p>
     </>
   );
+};
+
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired,
 };
 
 export default connect(null, { setAlert })(Register);
