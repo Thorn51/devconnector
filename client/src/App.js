ccf966494495cd64ac22b0f09ac3a01ca9aac52f
Matt Thornberry <mtberry16@hotmail.com>
2020-07-12 17:02:07 -0600

    Add load user

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../src/components/layout/Navbar";
 import Landing from "../src/components/layout/Landing";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
 import "./App.css";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
