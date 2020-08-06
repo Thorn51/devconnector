4e4ea946f5c2abd9c7fef53c15ae99c2c7c7af13
Matt Thornberry <mtberry16@hotmail.com>
2020-08-06 10:35:04 -0600

    Setup conditional to allow owner to edit profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
+import { Link } from "react-router-dom";
 
 const Profile = ({
   getProfileById,
@@ -16,7 +17,22 @@ const Profile = ({
 
   return (
     <Fragment>
-      {profile === null || loading ? <Spinner /> : <Fragment></Fragment>}
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
     </Fragment>
   );
 };
