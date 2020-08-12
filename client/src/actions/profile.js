c76a99c0645ff8189b3b80d8aba0c6281439eb29
Matt Thornberry <mtberry16@hotmail.com>
2020-08-12 08:37:09 -0600

    Fix comment

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -51,7 +51,7 @@ export const getProfiles = () => async (dispatch) => {
   }
 };
 
-// Get all profile by id
+// Get profile by id
 export const getProfileById = (userId) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
