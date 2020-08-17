4e35949513322e1a62e00472c8f56062de023d66
Matt Thornberry <mtberry16@hotmail.com>
2020-08-17 12:55:12 -0600

    Add jsx

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,12 +5,29 @@ import { getPosts } from "../../actions/post";
 
 // Components
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
